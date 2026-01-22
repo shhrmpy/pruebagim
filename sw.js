a081bd766669df923bc6b51e837d74691bb565fb
shhrmpy <maxito.aravena12@gmail.com>
2026-01-21 23:08:49 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'gymtracker-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activación y limpieza de cachés viejos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar peticiones (Estrategia: Network First, falling back to cache)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
